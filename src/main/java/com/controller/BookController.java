d9a332646600bb7d510be1beabf91a0dd6658848
Kranthi Kumar Vodnala <kranthi.vodnala1991@gmail.com>
2025-09-15 15:39:57 -0400

    Update BookController.java

--- src/main/java/com/controller/BookController.java
+++ src/main/java/com/controller/BookController.java
@@ -1,7 +1,7 @@
 package com.bookvault.controller;
 
 import com.bookvault.model.Book;
-import com.bookvault.repository.BookRepository;
+import com.bookvault.service.BookService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,29 +10,52 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController {
 
-    private final BookRepository bookRepo;
+    private final BookService bookService;
 
-    public BookController(BookRepository bookRepo) {
-        this.bookRepo = bookRepo;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
+    // Add a new book
     @PostMapping
     public Book addBook(@RequestBody Book book) {
-        return bookRepo.save(book);
+        return bookService.addBook(book);
     }
 
+    // Get all books
     @GetMapping
     public List<Book> getAllBooks() {
-        return bookRepo.findAll();
+        return bookService.getAllBooks();
     }
 
+    // Get books by author
     @GetMapping("/author/{author}")
     public List<Book> getByAuthor(@PathVariable String author) {
-        return bookRepo.findByAuthor(author);
+        return bookService.getBooksByAuthor(author);
     }
 
+    // Get books by genre
     @GetMapping("/genre/{genre}")
     public List<Book> getByGenre(@PathVariable String genre) {
-        return bookRepo.findByGenre(genre);
+        return bookService.getBooksByGenre(genre);
+    }
+
+    // Get book by ID
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+    }
+
+    // Update book by ID
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    // Delete book by ID
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
     }
 }
